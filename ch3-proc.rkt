2b2a80cfc0e8c43fe9046a2fab2e126f2437d61e
namo626 <namo26june@gmail.com>
2019-03-14 09:24:23 -0400

    Add PROC-lang specification

---
+++ ch3-proc.rkt
@@ -0,0 +1,129 @@
+#lang eopl
+
+(require "environment.rkt﻿")
+
+;; Parsing -> AST
+(define the-lexical-spec
+    '((whitespace (whitespace) skip)
+      (comment ("%" (arbno (not #\newline))) skip)
+      (identifier
+       (letter (arbno (or letter digit "_" "-" "?")))
+       symbol)
+      (number (digit (arbno digit)) number)
+      (number ("-" digit (arbno digit)) number)
+      ))
+
+(define the-grammar
+  '((program (expression) a-program)
+
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    (expression
+     ("proc" "(" identifier ")" expression)
+     proc-exp)
+
+    (expression
+     ("(" expression expression ")")
+     call-exp)
+
+    ))
+
+  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+
+
+;; Expressed value in PROC-lang
+
+(define-datatype expval expval?
+  [num-val (num number?)]
+  [bool-val (bool boolean?)]
+  [proc-val (proc proc?)])
+
+(define (expval->proc val)
+  (cases expval val
+         [proc-val (proc) proc]
+         [else (eopl:error 'expval->proc "Expval is not a procedure")]))
+
+;; Representation of procedures in Scheme (not the expressed value)
+
+(define (proc? val)
+  (procedure? val))
+
+(define (procedure var body env)
+  (lambda (val)
+    (value-of body (extend-env var val env))))
+
+(define (apply-procedure proc1 val)
+  (proc1 val))
+
+
+;; The interpreter
+;; value-of-program : Program -> ExpVal
+(define value-of-program
+  (lambda (pgm)
+    (cases program pgm
+           (a-program (exp1)
+                      (value-of exp1 (init-env))))))
+
+;; value-of : Exp * Env -> ExpVal
+(define value-of
+  (lambda (exp env)
+    (cases expression exp
+           (const-exp (num) (num-val num))
+           (var-exp (var) (apply-env env var))
+           (diff-exp (exp1 exp2)
+                     (let ((val1 (value-of exp1 env))
+                           (val2 (value-of exp2 env)))
+                       (let ((num1 (expval->num val1))
+                             (num2 (expval->num val2)))
+                         (num-val
+                          (- num1 num2)))))
+           (zero?-exp (exp1)
+                      (let ((val1 (value-of exp1 env)))
+                        (let ((num1 (expval->num val1)))
+                          (if (zero? num1)
+                              (bool-val #t)
+                              (bool-val #f)))))
+           (if-exp (exp1 exp2 exp3)
+                   (let ((val1 (value-of exp1 env)))
+                     (if (expval->bool val1)
+                         (value-of exp2 env)
+                         (value-of exp3 env))))
+           (let-exp (var exp1 body)
+                    (let ((val1 (value-of exp1 env)))
+                      (value-of body
+                                (extend-env var val1 env))))
+           (proc-exp (var body)
+                     (proc-val (procedure var body env)))
+           (call-exp (rator rand)
+                     (let ((proc (expval->proc (value-of rator env)))
+                           (arg (value-of rand env)))
+                       (apply-procedure proc arg))))))
